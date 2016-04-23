ee2a9b4a12594489a128ec0593ed440327fe6a43
Thomas Jollans <tjol@tjol.eu>
2016-04-23 20:01:34 +0200

    Updated code/ from topocm/topocm_content

--- code/init_mooc_nb.py
+++ code/init_mooc_nb.py
@@ -1,13 +1,8 @@
 import sys
 import os
 import re
-from types import SimpleNamespace
-
-# Enable inline plotting in the notebook
-try:
-    get_ipython().enable_matplotlib(gui='inline')
-except NameError:
-    pass
+import types
+import warnings
 
 import numpy as np
 import matplotlib
@@ -26,12 +21,17 @@ from edx_components import *
 import functions
 from functions import *
 
+init_mooc_nb = ['np', 'matplotlib', 'kwant', 'holoviews', 'init_notebook',
+                'interact', 'display_html', 'plt', 'pf',
+                'SimpleNamespace', 'pprint_matrix', 'scientific_number',
+                'pretty_fmt_complex']
 
-__all__ = ['np', 'matplotlib', 'kwant', 'holoviews',
-           'init_notebook', 'interact', 'display_html', 'plt', 'pf',
-           'SimpleNamespace', 'pprint_matrix', 'scientific_number',
-           'pretty_fmt_complex'] + edx_components.__all__ + functions.__all__
+__all__ = init_mooc_nb + edx_components.__all__ + functions.__all__
 
+class SimpleNamespace(types.SimpleNamespace):
+    def update(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return self
 
 # Adjust printing of matrices, and numpy printing of numbers.
 def pprint_matrix(data, digits=3):
@@ -112,16 +112,37 @@ $(document).ready(function (){if(!("IPython" in window)){onoff();}})
 </script>"""
 
 
-def init_notebook():
-    print('Populated the namespace with:\n' + ', '.join(__all__))
-    holoviews.notebook_extension('matplotlib')
+def init_notebook(mpl=True):
+    # Enable inline plotting in the notebook
+    if mpl:
+        try:
+            get_ipython().enable_matplotlib(gui='inline')
+        except NameError:
+            pass
 
+    print('Populated the namespace with:\n' +
+        ', '.join(init_mooc_nb) +
+        '\nfrom code/edx_components:\n' +
+        ', '.join(edx_components.__all__) +
+        '\nfrom code/functions:\n' +
+        ', '.join(functions.__all__))
+
+    holoviews.notebook_extension('matplotlib')
+    holoviews.plotting.mpl.MPLPlot.fig_rcparams['text.usetex'] = True
+    
+    latex_packs = [r'\usepackage{amsmath}',
+                   r'\usepackage{amssymb}'
+                   r'\usepackage{bm}']
+
+    holoviews.plotting.mpl.MPLPlot.fig_rcparams['text.latex.preamble'] = latex_packs
+
+    # Set plot style.
     options = Store.options(backend='matplotlib')
     options.Contours = Options('style', linewidth=2, color='k')
     options.Contours = Options('plot', aspect='square')
     options.HLine = Options('style', linestyle='--', color='b', linewidth=2)
     options.VLine = Options('style', linestyle='--', color='r', linewidth=2)
-    options.Image = Options('style', cmap='gist_heat_r')
+    options.Image = Options('style', cmap='RdBu_r')
     options.Image = Options('plot', title_format='{label}')
     options.Path = Options('style', linewidth=1.2, color='k')
     options.Path = Options('plot', aspect='square', title_format='{label}')
@@ -129,8 +150,13 @@ def init_notebook():
     options.Curve = Options('plot', aspect='square', title_format='{label}')
     options.Overlay = Options('plot', show_legend=False, title_format='{label}')
     options.Layout = Options('plot', title_format='{label}')
+    options.Surface = Options('style', cmap='RdBu_r', rstride=1, cstride=1, lw=0.2)
+    options.Surface = Options('plot', azimuth=20, elevation=8)
 
-    # Set plot style.
+    # Turn off a bogus holoviews warning.
+    # Temporary solution to ignore the warnings
+    warnings.filterwarnings('ignore', r'All-NaN (slice|axis) encountered')
+
     module_dir = os.path.dirname(__file__)
     matplotlib.rc_file(os.path.join(module_dir, "matplotlibrc"))
 
@@ -150,3 +176,7 @@ def init_notebook():
 
     display_html(display.HTML(nb_html_header +
                               (hide_outside_ipython if developer else '')))
+
+    # Patch a bug in holoviews
+    from patch_holoviews import patch_all
+    patch_all()
